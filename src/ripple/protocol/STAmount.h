d53607598b1cd905235af76a2691be8487f6d23e
Howard Hinnant <howard.hinnant@gmail.com>
2022-06-01 15:51:00 -0400

    Use Number for IOUAmount and STAmount arithmetic
    * Guarded by amendment fixUniversalNumber
    * Produces slightly better accuracy in some computations.

--- src/ripple/protocol/STAmount.h
+++ src/ripple/protocol/STAmount.h
@@ -280,7 +280,13 @@ private:
     STBase*
     move(std::size_t n, void* buf) override;
 
+    STAmount&
+    operator=(IOUAmount const& iou);
+
     friend class detail::STVar;
+
+    friend STAmount
+    operator+(STAmount const& v1, STAmount const& v2);
 };
 
 //------------------------------------------------------------------------------
