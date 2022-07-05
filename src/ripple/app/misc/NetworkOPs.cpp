b84f3444a5ea09ec6c7808e599ac8a752ed1c162
greg7mdp <greg7mdp@gmail.com>
2022-07-05 11:25:53 -0400

    implement RPC commands for accessing XChain ledger objects and their JTX tests
    add RPC command for ClainID and the JTX tests for it
    
    run clang-format
    
    RXI-290: Modify the account_history_tx_stream RPC command to add a field that returns when we reach a ledger boundary
    
    RXI-286 - Add new filter to  RPC command for xchain seq number
    
    RPC commands update - wip
    
    Updated RPC commands and tests to new names
    
    RPC requests and tests for xchain bridge - wip
    
    Factor out some common objects for XChainBridge testing
    
    move the two helper functions attestationClaimBatch and attestationCreateAccountBatch to a common header
    
    add some objects and clean up xchain_utils
    
    Update xchain_utils
    
    Finish tests for xchain RPC commands

--- src/ripple/app/misc/NetworkOPs.cpp
+++ src/ripple/app/misc/NetworkOPs.cpp
@@ -625,12 +625,14 @@ private:
     void
     pubValidatedTransaction(
         std::shared_ptr<ReadView const> const& ledger,
-        AcceptedLedgerTx const& transaction);
+        AcceptedLedgerTx const& transaction,
+        bool last);
 
     void
     pubAccountTransaction(
         std::shared_ptr<ReadView const> const& ledger,
-        AcceptedLedgerTx const& transaction);
+        AcceptedLedgerTx const& transaction,
+        bool last);
 
     void
     pubProposedAccountTransaction(
@@ -2971,7 +2973,8 @@ NetworkOPsImp::pubLedger(std::shared_ptr<ReadView const> const& lpAccepted)
     for (auto const& accTx : *alpAccepted)
     {
         JLOG(m_journal.trace()) << "pubAccepted: " << accTx->getJson();
-        pubValidatedTransaction(lpAccepted, *accTx);
+        pubValidatedTransaction(
+            lpAccepted, *accTx, accTx == *(--alpAccepted->end()));
     }
 }
 
@@ -3078,7 +3081,8 @@ NetworkOPsImp::transJson(
 void
 NetworkOPsImp::pubValidatedTransaction(
     std::shared_ptr<ReadView const> const& ledger,
-    const AcceptedLedgerTx& transaction)
+    const AcceptedLedgerTx& transaction,
+    bool last)
 {
     auto const& stTxn = transaction.getTxn();
 
@@ -3127,13 +3131,14 @@ NetworkOPsImp::pubValidatedTransaction(
     if (transaction.getResult() == tesSUCCESS)
         app_.getOrderBookDB().processTxn(ledger, transaction, jvObj);
 
-    pubAccountTransaction(ledger, transaction);
+    pubAccountTransaction(ledger, transaction, last);
 }
 
 void
 NetworkOPsImp::pubAccountTransaction(
     std::shared_ptr<ReadView const> const& ledger,
-    AcceptedLedgerTx const& transaction)
+    AcceptedLedgerTx const& transaction,
+    bool last)
 {
     hash_set<InfoSub::pointer> notify;
     int iProposed = 0;
@@ -3238,6 +3243,9 @@ NetworkOPsImp::pubAccountTransaction(
             RPC::insertDeliveredAmount(jvObj[jss::meta], *ledger, stTxn, meta);
         }
 
+        if (last)
+            jvObj[jss::account_history_ledger_boundary] = true;
+        
         for (InfoSub::ref isrListener : notify)
             isrListener->send(jvObj, true);
 
@@ -3639,8 +3647,11 @@ NetworkOPsImp::addAccountHistoryJob(SubAccountHistoryInfoWeak subInfo)
 
                     auto const& txns = dbResult->first;
                     marker = dbResult->second;
-                    for (auto const& [tx, meta] : txns)
+                    size_t num_txns = txns.size();
+                    for (size_t i = 0; i < num_txns; ++i)
                     {
+                        auto const& [tx, meta] = txns[i];
+
                         if (!tx || !meta)
                         {
                             JLOG(m_journal.debug())
@@ -3675,6 +3686,11 @@ NetworkOPsImp::addAccountHistoryJob(SubAccountHistoryInfoWeak subInfo)
                             *stTxn, meta->getResultTER(), true, curTxLedger);
                         jvTx[jss::meta] = meta->getJson(JsonOptions::none);
                         jvTx[jss::account_history_tx_index] = txHistoryIndex--;
+
+                        if (i + 1 == num_txns ||
+                            txns[i + 1].first->getLedger() != tx->getLedger())
+                            jvTx[jss::account_history_ledger_boundary] = true;
+
                         RPC::insertDeliveredAmount(
                             jvTx[jss::meta], *curTxLedger, stTxn, *meta);
                         if (isFirstTx(tx, meta))
