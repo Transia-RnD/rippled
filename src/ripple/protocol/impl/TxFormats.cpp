558ae6569e9e2fa14788ad0b7772e1bf980c7e4e
Denis Angell <dangell@transia.co>
2023-02-03 04:12:09 -0500

    clang-format

--- src/ripple/protocol/impl/TxFormats.cpp
+++ src/ripple/protocol/impl/TxFormats.cpp
@@ -330,13 +330,11 @@ TxFormats::TxFormats()
 
     add(jss::URIToken,
         ttURI_TOKEN,
-        {
-            {sfURI, soeOPTIONAL},
-            {sfURITokenID, soeOPTIONAL},
-            {sfAmount, soeOPTIONAL},
-            {sfDigest, soeOPTIONAL},
-            {sfDestination, soeOPTIONAL}
-        },
+        {{sfURI, soeOPTIONAL},
+         {sfURITokenID, soeOPTIONAL},
+         {sfAmount, soeOPTIONAL},
+         {sfDigest, soeOPTIONAL},
+         {sfDestination, soeOPTIONAL}},
         commonFields);
 }
 
