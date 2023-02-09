0829b1450cd02a57560c5909c1fe7eed3ab429c9
Denis Angell <dangell@transia.co>
2023-02-09 17:58:31 -0500

    clang-format
    clang-format
    
    clang-format
    
    clang-format

--- src/ripple/protocol/impl/TxFormats.cpp
+++ src/ripple/protocol/impl/TxFormats.cpp
@@ -359,7 +359,7 @@ TxFormats::TxFormats()
             {sfDestination, soeOPTIONAL},
         },
         commonFields);
-    
+
     add(jss::URITokenCancelSellOffer,
         ttURITOKEN_CANCEL_SELL_OFFER,
         {
