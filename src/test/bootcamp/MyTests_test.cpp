44cf66c09866398c81e8d869b26795cf2e8ddac9
Denis Angell <dangell@transia.co>
2025-07-16 14:17:26 +0200

    sql test

--- src/test/bootcamp/MyTests_test.cpp
+++ src/test/bootcamp/MyTests_test.cpp
@@ -1,5 +1,6 @@
 #include <test/jtx.h>
 #include <xrpl/beast/unit_test.h>
+#include <xrpld/app/rdb/backend/SQLiteDatabase.h>
 
 namespace ripple {
 namespace test {
@@ -82,13 +83,32 @@ class MyTests_test : public beast::unit_test::suite
         BEAST_EXPECT(1 == 1);
     }
 
+    void
+    testSql(FeatureBitset features)
+    {
+        testcase("SQL Test");
+
+        using namespace jtx;
+        
+        Env env{*this, features};
+        Account const alice{"alice"};
+        Account const bob{"bob"};
+        env.fund(XRP(5000), alice, bob);
+        env.close();
+
+        auto const db = dynamic_cast<SQLiteDatabase*>(&env.app().getRelationalDatabase());
+        auto const ledgerSeq = db->getMaxLedgerSeq();
+        BEAST_EXPECT(*ledgerSeq == 3);
+    }
+
     void 
     run() override
     {
         using namespace test::jtx;
         auto const sa = supported_amendments();
         // testProtocol(sa);
-        testTraceTransactor(sa);
+        // testTraceTransactor(sa);
+        testSql(sa);
     }
 };
 
