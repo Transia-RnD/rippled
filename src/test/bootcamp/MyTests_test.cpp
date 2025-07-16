f701af3c0100a351d17450c617b7de7c02e9b8c0
Denis Angell <dangell@transia.co>
2025-07-16 14:19:29 +0200

    clang-format

--- src/test/bootcamp/MyTests_test.cpp
+++ src/test/bootcamp/MyTests_test.cpp
@@ -1,6 +1,8 @@
 #include <test/jtx.h>
-#include <xrpl/beast/unit_test.h>
+
 #include <xrpld/app/rdb/backend/SQLiteDatabase.h>
+
+#include <xrpl/beast/unit_test.h>
 
 namespace ripple {
 namespace test {
@@ -18,15 +20,16 @@ class MyTests_test : public beast::unit_test::suite
         Env env{*this, features};
         auto const baseFee = env.current()->fees().base;
         env.fund(XRP(100000), alice, bob);
-        env.close(); // close the ledger
+        env.close();  // close the ledger
 
         auto const preAlice = env.balance(alice);
         std::cout << "Pre-Alice Balance: " << preAlice << std::endl;
 
-        auto jt = env.jt(escrow::create(alice, bob, XRP(1)), 
+        auto jt = env.jt(
+            escrow::create(alice, bob, XRP(1)),
             escrow::finish_time(env.now() + 3s),
             escrow::cancel_time(env.now() + 4s));
-        
+
         Serializer s;
         jt.stx->add(s);
         auto const seq = jt.stx->getFieldU32(sfSequence);
@@ -36,19 +39,21 @@ class MyTests_test : public beast::unit_test::suite
         std::cout << "Amount: " << amt.getJson(JsonOptions::none) << std::endl;
         BEAST_EXPECT(amt == XRP(1));
         std::cout << "Transaction: " << jt.jv << std::endl;
-        env(jt); // submits to ledger
-        env.close(); // close the ledger
+        env(jt);      // submits to ledger
+        env.close();  // close the ledger
 
         BEAST_EXPECT(env.balance(alice) == preAlice - XRP(1) - baseFee);
         std::cout << "Post-Alice Balance: " << env.balance(alice) << std::endl;
-        std::cout << "Post-Alice Balance=: " << (preAlice - XRP(1) - baseFee) << std::endl;
+        std::cout << "Post-Alice Balance=: " << (preAlice - XRP(1) - baseFee)
+                  << std::endl;
 
         auto const k = keylet::escrow(alice, seq);
         auto const sle = env.current()->read(k);
         BEAST_EXPECT(sle);
 
         auto amtSle = sle->getFieldAmount(sfAmount);
-        std::cout << "SLE Amount: " << amtSle.getJson(JsonOptions::none) << std::endl;
+        std::cout << "SLE Amount: " << amtSle.getJson(JsonOptions::none)
+                  << std::endl;
         BEAST_EXPECT(amtSle == XRP(1));
 
         {
@@ -68,7 +73,7 @@ class MyTests_test : public beast::unit_test::suite
         testcase("Your Transaction Test");
 
         using namespace jtx;
-        
+
         Env env{*this, features};
         Account const alice{"alice"};
         Account const bob{"bob"};
@@ -89,19 +94,20 @@ class MyTests_test : public beast::unit_test::suite
         testcase("SQL Test");
 
         using namespace jtx;
-        
+
         Env env{*this, features};
         Account const alice{"alice"};
         Account const bob{"bob"};
         env.fund(XRP(5000), alice, bob);
         env.close();
 
-        auto const db = dynamic_cast<SQLiteDatabase*>(&env.app().getRelationalDatabase());
+        auto const db =
+            dynamic_cast<SQLiteDatabase*>(&env.app().getRelationalDatabase());
         auto const ledgerSeq = db->getMaxLedgerSeq();
-        BEAST_EXPECT(*ledgerSeq == 3);
+        BEAST_EXPECT(*ledgerSeq == (env.current()->seq() - 1));
     }
 
-    void 
+    void
     run() override
     {
         using namespace test::jtx;
